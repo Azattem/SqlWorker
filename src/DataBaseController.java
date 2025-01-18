bb91f4b28cb549585eb2082c73fa96b67a1137bc
azat <azatteam2013@yandex.ru>
2025-01-18 08:11:27 +0500

    Доработал DataBaseController

--- src/DataBaseController.java
+++ src/DataBaseController.java
@@ -4,21 +4,54 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class DataBaseController {
-    private final Connection connection;
+    private Connection connection;
+    private final String url;
+    private final String login;
+    private final String pass;
     ArrayList<DataBaseAction> actionList = new ArrayList<>();
 
-    public DataBaseController(String Path, String login, String pass) throws SQLException {
-        String url = "jdbc:" + Path + "?useSSL=false&amp&serverTimezone=UTC";
-        connection = DriverManager.getConnection(url, login, pass);
+    public DataBaseController(String Path, String login, String pass) {
+        this.url="jdbc:" + Path + "?useSSL=false&amp&serverTimezone=UTC";
+        this.login=login;
+        this.pass=pass;
     }
-    
-
+    public void executeActions(){
+    for (DataBaseAction action : actionList) {
+    action.execute();
+    }
+    }
+    public void clear(){
+    actionList = new ArrayList<>();
+    }
+    public void remove(int index){
+    if(index>=0&&index<actionList.size()) {
+    actionList.remove(index);
+    }
+    }
+    public void remove(DataBaseAction action){
+            actionList.remove(action);
+    }
+    public void add(DataBaseAction dataBaseAction){
+        actionList.add(dataBaseAction);
+    }
+    //Метод подлючаеться по сылки на заранее заданую базу данных
+    public boolean connect(){
+        try {
+            connection = DriverManager.getConnection(url, login, pass);
+            return true;
+        } catch (SQLException e) {
+            System.out.println("Ошибка при обработке подключения");
+            return false;
+        }
+    }
     //Метод прерывает связь с БД
-    public void close(){
+    public boolean close(){
         try {
             connection.close();
+            return true;
         } catch (SQLException e) {
-        throw new RuntimeException(e);
+            System.out.println("Ошибка при обработке закрытия");
+        return false;
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,13 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            new DataBaseController("mysql://localhost:3306/test","root","qwerty1");
-            System.out.println("Подключение к базе данных успешно установлено!");
-
-        } catch (SQLException e) {
-            System.out.println("Ошибка при подключении к базе данных:");
-            e.printStackTrace();
-        }
+        DataBaseController s = new DataBaseController("mysql://localhost:3306/test","root","qwerty1");
+        s.connect();
+        s.add(new AddDataBaseAction(new DataRow("s")));
+        s.add(new AddDataBaseAction(new DataRow("s")));
+        s.add(new AddDataBaseAction(new DataRow("s")));
+        s.executeActions();
+        s.remove(3);
+        s.close();
     }
 }
